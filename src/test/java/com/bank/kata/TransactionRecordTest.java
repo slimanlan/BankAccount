f75afa8b315bdffcfd0a72c089ecd4e1f2038f48
slimane lansari <slimanlansari@gmail.com>
2020-07-13 08:34:57 +0200

    refactor code

--- src/test/java/com/bank/kata/TransactionRecordTest.java
+++ src/test/java/com/bank/kata/TransactionRecordTest.java
@@ -5,20 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TransactionRecordTest {
 	
+	TransactionRecord transactionRecord;
+	Account payer; 
+	Account payee; 
+	
+    @BeforeEach
+	public void setup() {
+		transactionRecord = new TransactionRecord();
+		payer = new Account(); 
+		payee = new Account();
+		payer.deposit(1000);
+	}
+	
 	@Test
 	public void test_save_transaction_payer_amount() {
 		
-		Account payer = new Account(); 
-		Account payee = new Account();
-		payer.deposit(1000);
-		TransactionRecord transactionRecord = new TransactionRecord(); 
-		
 		Transaction transaction = transactionRecord.saveTransaction(payer, payee, 50); 
-		
 		assertEquals(transaction.getPayer().getAmount() , 950); 
 		
 	}
@@ -26,13 +34,7 @@ public class TransactionRecordTest {
 	@Test
 	public void test_save_transaction_payee_amount() {
 		
-		Account payer = new Account(); 
-		Account payee = new Account();
-		payer.deposit(1000);
-		TransactionRecord transactionRecord = new TransactionRecord(); 
-		
 		Transaction transaction = transactionRecord.saveTransaction(payer, payee, 50); 
-		 
 		assertEquals(transaction.getPayee().getAmount() , 50);
 		
 	}
@@ -40,13 +42,7 @@ public class TransactionRecordTest {
 	@Test
 	public void test_save_transaction_amount() {
 		
-		Account payer = new Account(); 
-		Account payee = new Account();
-		payer.deposit(1000);
-		TransactionRecord transactionRecord = new TransactionRecord(); 
-		
 		Transaction transaction = transactionRecord.saveTransaction(payer, payee, 50); 
-		
 		assertEquals(transaction.getAmount() , 50);
 		
 	}
@@ -54,13 +50,7 @@ public class TransactionRecordTest {
 	@Test
 	public void test_save_transaction_from_payer_to_payee() {
 		
-		Account payer = new Account(); 
-		Account payee = new Account();
-		payer.deposit(1000);
-		TransactionRecord transactionRecord = new TransactionRecord(); 
-		
 		Transaction transaction = transactionRecord.saveTransaction(payer, payee, 50); 
-		
 		assertTrue(transaction.getPayer().equals(payer));
 		assertTrue(transaction.getPayee().equals(payee));
 		
@@ -69,51 +59,26 @@ public class TransactionRecordTest {
 	@Test
 	public void test_get_all_transaction_from_payer() {
 		
-		TransactionRecord transactionRecord = new TransactionRecord();
-
-		Account payer = new Account(); 
-		payer.deposit(10000);
-		
-		Account payee1 = new Account();		
-		transactionRecord.saveTransaction(payer, payee1, 50); 
-		
-		Account payee2 = new Account();		
-		transactionRecord.saveTransaction(payer, payee2, 150); 
-		
-		Account payee3 = new Account();		
-		transactionRecord.saveTransaction(payer, payee3, 250); 
+		transactionRecord.saveTransaction(payer, payee, 50); 
+		transactionRecord.saveTransaction(payer, payee, 150); 
+		transactionRecord.saveTransaction(payer, payee, 250); 
 		
 		List<Transaction> transactionRecordFromPayer = transactionRecord.getAllTransactionFromPayer(payer); 
 		
 		assertEquals(transactionRecordFromPayer.size() , 3);
-
 		transactionRecordFromPayer.forEach(transaction -> {
 			assertTrue(transaction.getPayer().equals(payer));
 		});
+		
 	}
 	
 	@Test
 	public void test_get_all_transaction_to_payee() {
 		
-		TransactionRecord transactionRecord = new TransactionRecord();
-
-		Account payee = new Account();
-		
-		Account payer1 = new Account();		
-		payer1.deposit(1000);
-		transactionRecord.saveTransaction(payer1, payee, 100); 
-		
-		Account payer2 = new Account();		
-		payer2.deposit(1000);
-		transactionRecord.saveTransaction(payer2, payee, 50); 
-		
-		Account payer3 = new Account();		
-		payer3.deposit(1000);
-		transactionRecord.saveTransaction(payer3, payee, 120); 
-		
-		Account payer4 = new Account();		
-		payer4.deposit(1000);
-		transactionRecord.saveTransaction(payer4, payee, 130); 
+		transactionRecord.saveTransaction(payer, payee, 100); 
+		transactionRecord.saveTransaction(payer, payee, 50); 
+		transactionRecord.saveTransaction(payer, payee, 120); 
+		transactionRecord.saveTransaction(payer, payee, 130); 
 		
 		List<Transaction> transactionRecordToPayee = transactionRecord.getAllTransactionToPayee(payee); 
 		
--- src/test/java/com/bank/kata/TransfertTest.java
+++ src/test/java/com/bank/kata/TransfertTest.java
@@ -4,71 +4,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TransfertTest {
 	
+	Account payer; 
+	Account payee;
+	Transfert transfert; 
+	
+    @BeforeEach
+	public void setup() {
+		payer = new Account(); 
+		payee = new Account();
+		transfert = new Transfert(payer, payee);
+		payer.deposit(500);
+	}
+	
+	
 	@Test
 	public void test_transfert_payer_amount() {
-		Account payer = new Account();
-		payer.deposit(500);
-		Account payee = new Account();
-	
-		Transfert transfert = new Transfert(payer, payee);
 		transfert.transfertAmountFromPayerToPayee(60);
-	
 		assertEquals(payer.getAmount(), 440);
 	}
 	
 	@Test
 	public void test_transfert_payee_amount() {
-		Account payer = new Account();
-		payer.deposit(500);
-		Account payee = new Account();
-	
-		Transfert transfert = new Transfert(payer, payee);
 		transfert.transfertAmountFromPayerToPayee(60);
-	
 		assertEquals(payee.getAmount(), 60);
 	}
 	
 	@Test
 	public void test_multipe_transfert_from_payer() {
-		Account payer = new Account();
-		payer.deposit(500);
-		
-		Account payee1 = new Account();
-		Account payee2 = new Account();
-		
-		Transfert transfert1 = new Transfert(payer, payee1);
-		transfert1.transfertAmountFromPayerToPayee(60);
-		
-		Transfert transfert2 = new Transfert(payer, payee2);
-		transfert2.transfertAmountFromPayerToPayee(30);
-	
+		transfert.transfertAmountFromPayerToPayee(60);
+		transfert.transfertAmountFromPayerToPayee(30);
 		assertEquals(payer.getAmount(), 410);
-		
 	}
 	
 	
 	@Test
 	public void test_multipe_transfert_to_payee() {
-		Account payer1 = new Account();
-		payer1.deposit(500);
-		
-		Account payer2 = new Account();
-		payer2.deposit(400);
-		
-		Account payee = new Account();
-		
-		Transfert transfert1 = new Transfert(payer1, payee);
-		transfert1.transfertAmountFromPayerToPayee(100);
-		
-		Transfert transfert2 = new Transfert(payer2, payee);
-		transfert2.transfertAmountFromPayerToPayee(40);
-	
+		transfert.transfertAmountFromPayerToPayee(100);
+		transfert.transfertAmountFromPayerToPayee(40);
 		assertEquals(payee.getAmount(), 140);
 	}
-	
-	
 }
